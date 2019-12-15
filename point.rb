0abd5ba3b1f946bd0a64b8e752a725349a2b9a0a
Yan Zalavin <yztrnc@gmail.com>
2019-12-15 02:36:12 +0200

    Add Comparable

--- point.rb
+++ point.rb
@@ -1,15 +1,9 @@
 # frozen_string_literal: true
 
 Point = Struct.new(:x,:y) do
-  def >(other)
-    y > other.y
-  end
+  include Comparable
 
-  def >=(other)
-    y >= other.y
-  end
-
-  def <=(other)
-    y <= other.y
+  def <=>(other)
+    y <=> other.y
   end
 end
